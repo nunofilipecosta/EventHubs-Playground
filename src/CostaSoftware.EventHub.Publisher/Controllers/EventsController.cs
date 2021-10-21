6a844ffa5aa16516bf7294a5dc35ecb7493252c2
Nuno Costa <nunofilipe.costa@gmail.com>
2021-10-21 16:00:38 +0100

    ADD : Project for sample publisher

---
+++ src/CostaSoftware.EventHub.Publisher/Controllers/EventsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CostaSoftware.EventHub.Publisher.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        public EventsController()
+        {
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            return Ok();
+        }
+    }
+}
